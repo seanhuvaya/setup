01410071591a627a433d0d5f7ee77866931a174f
Sean Huvaya <64103955+seanhuvaya@users.noreply.github.com>
2026-04-30 16:44:33 -0500

    Update setup script to modify installed applications
    Removed installation commands for Discord and Slack, and added installation command for Cursor. Also removed Notion Calendar installation command.

--- setup.sh
+++ setup.sh
@@ -16,24 +16,14 @@ eval "$(/opt/homebrew/bin/brew shellenv zsh)"
 echo -e "${GREEN}Installing item2...${NO_COLOR}"
 brew install --cask iterm2
 
-echo -e "${GREEN}Installing ohmyzsh...${NO_COLOR}"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo -e "${GREEN}Installing powerlevel10k theme...${NO_COLOR}"
-brew install powerlevel10k
-echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
-
 echo -e "${GREEN}Installing Brave Browser...${NO_COLOR}"
 brew install --cask brave-browser
 
-echo -e "${GREEN}Installing Discord...${NO_COLOR}"
-brew install --cask discord
-
-echo -e "${GREEN}Installing Slack...${NO_COLOR}"
-brew install --cask slack
-
 echo -e "${GREEN}Installing PyCharm...${NO_COLOR}"
 brew install --cask pycharm
+
+echo -e "${GREEN}Installing Cursor...${NO_COLOR}"
+brew install --cask cursor
 
 echo -e "${GREEN}Installing uv...${NO_COLOR}"
 brew install uv
@@ -44,11 +34,15 @@ brew install --cask rectangle
 echo -e "${GREEN}Installing Notion...${NO_COLOR}"
 brew install --cask notion
 
-echo -e "${GREEN}Installing Notion Calendar...${NO_COLOR}"
-brew install --cask notion-calendar
-
 echo -e "${GREEN}Installing Postman...${NO_COLOR}"
 brew install --cask postman
 
 echo -e "${GREEN}Installing Docker...${NO_COLOR}"
 brew install --cask docker
+
+echo -e "${GREEN}Installing ohmyzsh...${NO_COLOR}"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo -e "${GREEN}Installing powerlevel10k theme...${NO_COLOR}"
+brew install powerlevel10k
+echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
